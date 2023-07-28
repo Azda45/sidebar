a2aaed22534f3f50c6f1eb25594578b8377179a2
Azda45 <72027480+Azda45@users.noreply.github.com>
2023-07-28 09:46:43 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,9 +4,17 @@ function changeClassBasedOnCheckbox() {
 
   if (checkboxElement.checked) {
     divElement.classList.remove("container-red");
+    divElement.classList.remove("container-red");
+    divElement.classList.remove("container-red");
+    divElement.classList.add("container-black");
+    divElement.classList.add("container-black");
     divElement.classList.add("container-black");
   } else {
     divElement.classList.remove("container-black");
+    divElement.classList.remove("container-black");
+    divElement.classList.remove("container-black");
+    divElement.classList.add("container-red");
+    divElement.classList.add("container-red");
     divElement.classList.add("container-red");
   }
 }
