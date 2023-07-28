88696ab266d820f1b8a97c76f5acf4fe1e441565
Achmad zailani <growtopiagame875236@gmail.com>
2023-07-28 08:17:36 +0800

    beta-1.0

---
+++ script.js
@@ -0,0 +1,14 @@
+function changeClassBasedOnCheckbox() {
+  var checkboxElement = document.getElementById("myCheckbox");
+  var divElement = document.getElementById("container");
+
+  if (checkboxElement.checked) {
+    divElement.classList.remove("container-red");
+    divElement.classList.add("container-black");
+  } else {
+    divElement.classList.remove("container-black");
+    divElement.classList.add("container-red");
+  }
+}
+
+document.getElementById("myCheckbox").addEventListener("change", changeClassBasedOnCheckbox);
